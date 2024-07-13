83f409561e2984886a4a1a85caad5090f307a85b
Patrick Zhao <patrick.zhao1989@outlook.com>
2024-07-13 22:02:01 +1000

    initial commit

---
+++ JRDemo/Controllers/HelloWorld.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JRDemo.Controllers;
+
+[Route("[controller]")]
+public class HelloWorldController : Controller
+{
+	[HttpGet("/hello")]
+	public IActionResult GetHelloWorld()
+	{
+		return Ok("Hello, World!");
+	}
+}
+
+
+// [ApiController]
+//     [Route("[controller]")]
+//     public class HelloWorldController : ControllerBase
+//     {
+//         [HttpGet("hello")]
+//         public IActionResult GetHelloWorld()
+//         {
+//             return Ok("Hello, World!");
+//         }
+//     }
